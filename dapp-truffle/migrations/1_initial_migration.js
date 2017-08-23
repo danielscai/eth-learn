288874f21fd01f63597682b2b174fe20418dfbb0
yunfei.cai <yunfei.cai@datayes.com>
2017-08-23 13:02:22 +0000

    truffle migrate

--- dapp-truffle/migrations/1_initial_migration.js
+++ dapp-truffle/migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
-#var Migrations = artifacts.require("./Migrations.sol");
+//var Migrations = artifacts.require("./Migrations.sol");
 var Test = artifacts.require("./Test.sol");
 
 module.exports = function(deployer) {
-  #deployer.deploy(Migrations);
+  //deployer.deploy(Migrations);
   deployer.deploy(Test);
 };
